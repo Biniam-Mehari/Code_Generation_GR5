d420c22a0a40617591b79843a9965cc7b2171aee
Abhi6921 <54932316+Abhi6921@users.noreply.github.com>
2022-06-01 01:11:23 +0200

    static add transactions UI

---
+++ src/components/transactions/CreateTransaction.vue
@@ -0,0 +1,52 @@
+<template>
+    <section>
+    <div class="container">
+      <form ref="form">
+        <h2 class="mt-3 mt-lg-5">Create a transaction</h2>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">From account</span>
+          <input type="text" class="form-control" />
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">To account</span>
+          <input type="number" class="form-control" />
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">Amount in €:</span>
+          <input type="text" class="form-control" />
+        </div>
+
+        <div class="input-group mt-4">
+          <button type="button" class="btn btn-primary">Transfer</button>
+          <button type="button" class="btn btn-danger">Cancel</button>
+          <button type="button" class="btn btn-info">List all transactions</button>
+        </div>
+      </form>
+
+    </div>
+  </section>
+</template>
+
+<script>
+// if cancel button pressed then clear the textareas and values
+export default {
+    name: 'AddTransactions',
+    data() {
+        return {
+            "id": '',
+            "fromAccount": '',
+            "toAccount": '',
+            "amount": '',
+            "timestamp": ''
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,25 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import { createRouter, createWebHistory } from 'vue-router'
+
+
+// import Home from './components/Home.vue';
+//import CreateTransactions from './components/transactions/CreateTransactions'
+
+// const routes = [
+//     { path: '/', component: Home },
+//     { path: '/products', component: ProductList },
+//     { path: '/createproduct', component: CreateProduct },
+//     { path: '/editproduct/:id', component: EditProduct, props: true  }
+// ];
+
+// const router = createRouter({
+//     "history": createWebHistory(),
+//     routes
+// })
+
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+createApp(App).mount('#app')
 
-
-import Home from './components/Home.vue';
-
-
-const routes = [
-    { path: '/', component: Home },
-    { path: '/products', component: ProductList },
-    { path: '/createproduct', component: CreateProduct },
-    { path: '/editproduct/:id', component: EditProduct, props: true  }
-];
-
-const router = createRouter({
-    "history": createWebHistory(),
-    routes
-})
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
 
