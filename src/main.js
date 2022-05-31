59669515e5516b66beb43d6e4f7992b89c2cf948
B-student <Bingoleley2015@gmail.com>
2022-05-31 21:29:22 +0200

    start up router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+
+import Home from './components/Home.vue';
+
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/products', component: ProductList },
+    { path: '/createproduct', component: CreateProduct },
+    { path: '/editproduct/:id', component: EditProduct, props: true  }
+];
+
+const router = createRouter({
+    "history": createWebHistory(),
+    routes
+})
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
+
