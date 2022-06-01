58b6e5951ee534e91b8d700a56cab7208f080cd7
B-student <Bingoleley2015@gmail.com>
2022-06-01 20:23:54 +0200

    accounts

---
+++ src/components/Navigation.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <div class="container-fluid">
+      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <li class="nav-item">
+          <!-- Add a router link to the homepage (don't use the a tag!) -->
+          <router-link to="/" class="nav-link" active-class="active"
+            >My Accounts</router-link>
+        </li>      
+        <li class="nav-item">
+          <!-- add a router link to the products page (don't use the a tag!) -->
+          <router-link to="dc" class="nav-link" active-class="active"
+            >Create Transactions</router-link
+          >
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/accounts/CreateAccount.vue
@@ -0,0 +1,42 @@
+<template>
+  <form action="">
+    <div class="container px-3">
+      <h3>Create new account</h3>
+      
+        <label for="userid">User-Id </label>
+     <div class="divider"/>
+        <input class="userid" type="text" />
+    
+      <br><br>
+        
+          <label for="dropdown">Type of account </label>
+         
+          <div class="divider"/>
+          <select>
+            <option selected>Select account type</option>
+            <option value="1">Current</option>
+            <option value="2">Saving</option>
+          </select>
+         
+          <br><br>
+          
+          <button type="button" margin-right="10px" class="btn btn-success">Create</button>
+          <div class="divider"/>
+          <button type="button" class="btn btn-danger" @click="this.$router.push('/');">Cancel</button>
+          
+      </div>
+  
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.divider{
+    width:8px;
+    height:auto;
+    display:inline-block;
+}
+</style>
